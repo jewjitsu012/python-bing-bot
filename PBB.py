46aea07bfc5eb06243fb5fde669552b6bf853afd
justinxxvii <justinamberson@Justins-Mac-mini.local>
2014-08-23 08:36:26 -0500

    Added a new config option
    now must set chromedriver path in the config

--- PBB.py
+++ PBB.py
@@ -21,15 +21,17 @@ def main(argv=None):
     if argv is None:
     	argv = sys.argv
 	set_working_directory()
-	print "Initializing desktop browser"
-	desktopBrowser = webdriver.Chrome()
+	config = load_yaml_file('config.yaml')
+	path = config['CHROMEDRIVER_PATH'][0]
+	print "Initializing desktop browser at path " + path
+	desktopBrowser = webdriver.Chrome(path)
 	desktopBrowser.set_window_size(1280,1024)
 	config = load_yaml_file('config.yaml')
 	desktopBot = PBBDesktopBot.DesktopBot(desktopBrowser,config)
 	desktopBot.begin_searching()
 	options = webdriver.ChromeOptions()
 	options.add_argument('--user-agent=Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7')
-	mobileBrowser = webdriver.Chrome(chrome_options=options)
+	mobileBrowser = webdriver.Chrome(path,chrome_options=options)
 	print "Initializing mobile browser"
 	mobileBot = PBBMobileBot.MobileBot(mobileBrowser,config)
 	mobileBot.begin_searching()
